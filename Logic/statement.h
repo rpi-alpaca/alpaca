84c8cfccb420a4de39be1e7d1acf44a145bf5d0c
NYYuHao <NYYuHao@gmail.com>
2019-09-17 16:41:46 -0400

    Added documentation for statement class

--- Logic/statement.h
+++ Logic/statement.h
@@ -6,10 +6,16 @@
 
 class Statement {
 private:
+	//Input statement is the statement to be interpreted from user input
 	std::string input_statement;
+
+	//"v" for variable
+	//"a" for and
+	//"o" for or
 	char statement_type;
 	bool value;
 
+	//Interpreter for input_statment
 	void interpretStatement();
 public:
 	Statement();
