a2263e815a58df0f5c92e9238f033e00f6fb0a64
Prasant Acharya <setinadi@gmail.com>
2019-09-18 08:31:46 -0400

    Update main.py

--- GUI/main.py
+++ GUI/main.py
@@ -1,3 +1,28 @@
 # this is the python file
 # Gitpod test commit!
+<<<<<<< HEAD
+=======
+
+# This file is the main code for the GUI for ALPACA. It acts as
+#	the viewer in the MVC structure, and it allows interaction
+#	with the logic engine
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.graphics import *
+from kivy.uix.image import Image
+
+class AlpacaLogo(Image):
+	pass
+
+class Alpaca(Widget):
+	pass
+
+class AlpacaApp(App):
+	def build(self):
+		return Alpaca()
+
+if __name__ == "__main__":
+	AlpacaApp().run()
+>>>>>>> GUI
 
