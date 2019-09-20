57bedfd81d98cd0038f42aaad51b9819e44840a2
TLouison <toddmlouison@gmail.com>
2019-09-20 16:43:43 -0400

    Removed big declaration string from main.py
    Before, we had a giant kivy builder string to define what was in each screen,
    but now it is all abstracted out to the alpaca.kv file so the code is cleaner.

--- GUI/main.py
+++ GUI/main.py
@@ -11,63 +11,6 @@ from kivy.graphics import *
 from kivy.uix.image import Image
 from kivy.uix.screenmanager import ScreenManager, Screen
 
-# Create both screens. Please note the root.manager.current: this is how
-# you can control the ScreenManager from kv. Each screen has by default a
-# property manager that gives you the instance of the ScreenManager used.
-Builder.load_string("""
-<MenuScreen>:
-    FloatLayout:
-        BoxLayout:
-            Button:
-                text: 'Goto help'
-                on_press: 
-                    root.manager.transition.direction = 'up'
-                    root.manager.current = 'Help'			
-            Button:
-                text: 'Goto new project'
-                on_press: 
-                    root.manager.transition.direction = 'up'
-                    root.manager.current = 'NewProject'	
-            Button:
-                text: 'Goto existing project'
-                on_press: 
-                    root.manager.transition.direction = 'up'
-                    root.manager.current = 'ExistingProject'
-
-<HelpScreen>:
-    FloatLayout:
-        BoxLayout:
-            Label:
-                text: "Help screen"
-            Button:
-                text: 'Back to menu'
-                on_press: 
-                    root.manager.transition.direction = 'down'
-                    root.manager.current = 'Menu'
-
-<NewProjectScreen>:
-    FloatLayout:
-        BoxLayout:
-            Label:
-                text: "New project screen"
-            Button:
-                text: 'Back to menu'
-                on_press: 
-                    root.manager.transition.direction = 'down'
-                    root.manager.current = 'Menu'
-
-<ExistingProjectScreen>:
-    FloatLayout:
-        BoxLayout:
-            Label:
-                text: "Existing project screen"
-            Button:
-                text: 'Back to menu'
-                on_press: 
-                    root.manager.transition.direction = 'down'
-                    root.manager.current = 'Menu'
-""")
-
 class HelpScreen(Screen):
 	pass
 
@@ -80,17 +23,16 @@ class ExistingProjectScreen(Screen):
 class MenuScreen(Screen):
 	pass
 
-# Create the screen manager
-sm = ScreenManager()
-sm.add_widget(MenuScreen(name='Menu'))
-sm.add_widget(HelpScreen(name='Help'))
-sm.add_widget(NewProjectScreen(name='NewProject'))
-sm.add_widget(ExistingProjectScreen(name='ExistingProject'))
-sm.current = 'Menu'
-
 class AlpacaApp(App):
-	def build(self):
-		return sm
+    def build(self):
+        # Create the screen manager
+        sm = ScreenManager()
+        sm.add_widget(MenuScreen(name='Menu'))
+        sm.add_widget(HelpScreen(name='Help'))
+        sm.add_widget(NewProjectScreen(name='NewProject'))
+        sm.add_widget(ExistingProjectScreen(name='ExistingProject'))
+        sm.current = 'Menu'
+        return sm
 
 if __name__ == "__main__":
 	AlpacaApp().run()
