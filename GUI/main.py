1a2478a586a51edb2b1f47fef764e8d8fcdf4af9
TLouison <toddmlouison@gmail.com>
2019-09-20 16:37:16 -0400

    Created home screen with navigation
    Built a homescreen that has 3 buttons with the basic navigation ability to
    switch the screen we are currently looking at. This is the jumping off point
    for all other devlopment of the GUI as we now have a basis to work on.

--- GUI/main.py
+++ GUI/main.py
@@ -2,20 +2,95 @@
 #	the viewer in the MVC structure, and it allows interaction
 #	with the logic engine
 
+from sys import exit
 from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 from kivy.graphics import *
 from kivy.uix.image import Image
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-class AlpacaLogo(Image):
+# Create both screens. Please note the root.manager.current: this is how
+# you can control the ScreenManager from kv. Each screen has by default a
+# property manager that gives you the instance of the ScreenManager used.
+Builder.load_string("""
+<MenuScreen>:
+    FloatLayout:
+        BoxLayout:
+            Button:
+                text: 'Goto help'
+                on_press: 
+                    root.manager.transition.direction = 'up'
+                    root.manager.current = 'Help'			
+            Button:
+                text: 'Goto new project'
+                on_press: 
+                    root.manager.transition.direction = 'up'
+                    root.manager.current = 'NewProject'	
+            Button:
+                text: 'Goto existing project'
+                on_press: 
+                    root.manager.transition.direction = 'up'
+                    root.manager.current = 'ExistingProject'
+
+<HelpScreen>:
+    FloatLayout:
+        BoxLayout:
+            Label:
+                text: "Help screen"
+            Button:
+                text: 'Back to menu'
+                on_press: 
+                    root.manager.transition.direction = 'down'
+                    root.manager.current = 'Menu'
+
+<NewProjectScreen>:
+    FloatLayout:
+        BoxLayout:
+            Label:
+                text: "New project screen"
+            Button:
+                text: 'Back to menu'
+                on_press: 
+                    root.manager.transition.direction = 'down'
+                    root.manager.current = 'Menu'
+
+<ExistingProjectScreen>:
+    FloatLayout:
+        BoxLayout:
+            Label:
+                text: "Existing project screen"
+            Button:
+                text: 'Back to menu'
+                on_press: 
+                    root.manager.transition.direction = 'down'
+                    root.manager.current = 'Menu'
+""")
+
+class HelpScreen(Screen):
 	pass
 
-class Alpaca(Widget):
+class NewProjectScreen(Screen):
 	pass
+
+class ExistingProjectScreen(Screen):
+    pass
+
+class MenuScreen(Screen):
+	pass
+
+# Create the screen manager
+sm = ScreenManager()
+sm.add_widget(MenuScreen(name='Menu'))
+sm.add_widget(HelpScreen(name='Help'))
+sm.add_widget(NewProjectScreen(name='NewProject'))
+sm.add_widget(ExistingProjectScreen(name='ExistingProject'))
+sm.current = 'Menu'
 
 class AlpacaApp(App):
 	def build(self):
-		return Alpaca()
+		return sm
 
 if __name__ == "__main__":
 	AlpacaApp().run()
