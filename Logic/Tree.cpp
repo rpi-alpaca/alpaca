79bc7886b13b69bb063b6d3e6ce22f601d73d4cc
NYYuHao <NYYuHao@gmail.com>
2019-09-24 16:32:24 -0400

    Fixed this issues

--- Logic/Tree.cpp
+++ Logic/Tree.cpp
@@ -1,11 +1,12 @@
 #include "Tree.h"
 
 StatementParser::StatementParser(){
-	this->head = nullptr;
+	head = nullptr;
 }
 
 StatementParser::StatementParser(const StatementParser& s1, const StatementParser& s2) {
-
+	head = new StatementNode();
+	
 }
 
 StatementParser::StatementParser(const std::string& statement){
