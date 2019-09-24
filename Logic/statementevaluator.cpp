954677dc8419667fd0d7024aa516dfba14fda091
Hali Cai <caippx@hotmail.com>
2019-09-24 17:16:35 -0400

    Fixed syntax errors in statementeval.cpp

--- Logic/statementevaluator.cpp
+++ Logic/statementevaluator.cpp
@@ -1,19 +1,24 @@
 
 #include "statement.h"
 #include "statementevaluator.h"
+#include "Tree.h"
 
 // CONSTRUCTOR		-	Initializes functionMap with characters mapped to the corresponding functions.
 StatementEvaluator::StatementEvaluator(){
-	functionMap['|'] = [](bool p, bool q){return p || q;};
-	functionMap['&'] = [](bool p, bool q){return p && q;};
-	functionMap['~'] = [](bool p){return !p;};
+	//functionMap['|'] = [](bool p, bool q){return p || q;};
+	//functionMap['&'] = [](bool p, bool q){return p && q;};
+	//functionMap['~'] = [](bool p){return !p;};
 }
 
 
-bool StatementEvaluator::evaluateStatement(const Statement& s, const std::vector<int> variableTruthValues) const{
+bool StatementEvaluator::evaluateStatement(const StatementParser& s, const std::vector<int> variableTruthValues) const{
 	
 }
 
+
+bool StatementEvaluator::evaluateBranch(const StatementNode& p){
+
+}
 
 void StatementEvaluator::printTruthTree(const Statement& s) const{
 
--- Logic/statementevaluator.h
+++ Logic/statementevaluator.h
@@ -9,11 +9,13 @@ class StatementEvaluator{
 	public:
 		StatementEvaluator();
 		bool evaluateStatement(const Statement& s, const std::vector<int> variableTruthValues) const;
-		void printTruthTree(const Statement& s) const;
+		void printTruthTree(const StatementParser& s) const;
 
 	private:
 		//Mapping of operation characters to the corresponding functions.
 		std::unordered_map<char, std::function<bool (bool, bool)> > functionMap();
+
+		bool evaluateBranch(const StatementNode& p) const;
 }
 
 #endif
