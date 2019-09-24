41c263be63a3ae8777dcbfc05037d030b9f47f53
NYYuHao <NYYuHao@gmail.com>
2019-09-24 16:24:59 -0400

    merge fixes

--- Logic/Tree.h
+++ Logic/Tree.h
@@ -2,17 +2,20 @@
 
 class StatementNode {
 public:
+	StatementNode() : left(NULL), right(NULL), value(NULL) {}
+
 	StatementNode* left;
 	StatementNode* right;
 	std::string value
 };
 
 class StatementParser {
-private:
+public: 
 	StatementParser();
+	StatementParser(StatementParser s1, StatementParser s2);
 	StatementParser(std::string statement);
-	
 
+private:
 	StatementNode* Head;
 
 }
