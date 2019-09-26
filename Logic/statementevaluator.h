faa825fe6d50496dda9d81b3d18465ff35c8f79a
Hali Cai <caippx@hotmail.com>
2019-09-25 21:00:35 -0400

    Modified statementevaluator.cpp/.h

--- Logic/statementevaluator.h
+++ Logic/statementevaluator.h
@@ -1,6 +1,6 @@
 
-#ifndef __STATEMENT_EVALUATOR_H_
-#define __STATEMENT_EVALUATOR_H_
+#ifndef _STATEMENT_EVALUATOR_H_
+#define _STATEMENT_EVALUATOR_H_
 
 #include "Tree.h"
 #include <functional>
@@ -9,14 +9,17 @@
 
 class StatementEvaluator{
 	public:
-		bool evaluateStatement(const StatementParser& s, const std::vector<int> variableTruthValues);
-		void printTruthTree(const StatementParser& s) const;
+		//bool evaluateStatement(const StatementParser& s, const std::vector<int>& variableTruthValues) const;
 
 	private:
 		//Mapping of operation characters to the corresponding functions.
-		//std::unordered_map<char, std::function<bool (bool, bool)> > functionMap();
+		std::unordered_map<char, std::function<bool (bool, bool)> > functionMap = {
+			{'|', [](bool p, bool q){return p || q;}},
+			{'&', [](bool p, bool q){return p && q;}},
+			{'@', [](bool p, bool q){return p xor q;}}
+		};
 
-		bool evaluateBranch(const StatementNode& p) const;
+		//bool evaluateBranch(const StatementNode& p, const std::vector<int>& variableTruthValues) const;
 };
 
 #endif
