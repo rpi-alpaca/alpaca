2f80d714335295d287f45db75035c44c313f290c
hsueha <alexandrahsueh@gmail.com>
2019-10-01 16:47:25 -0400

    Started up statement parser for with parentheses

--- Logic/Tree.cpp
+++ Logic/Tree.cpp
@@ -14,10 +14,41 @@ StatementParser::StatementParser(const std::string& statement){
 	//split it up
 	head = new StatementNode();
 	head -> value = statement;
-	if (statement.find('(') >= 0){
+	int firstParen = statement.find('(');
+	if (firstParen != std::string::npos){
 		//Note to self: maybe we should count how many parentheses there are to check if the input is valid?
 		std::string stat1 = "";
 		std::string stat2 = "";
+		if(firstParen == 0){
+			int nextStart = 1;
+			int nextEnd = 1;
+			bool contin = true;
+			while(contin){
+				int nextParen = statement.find('(', nextStart);
+				int nextClosed = statement.find(')', nextEnd)
+				if(nextClosed = std::string::npos){
+					//error
+					System.out.println("Error: Incorrect inputs")
+					break;
+				}
+				if(nextParen != std::string::npos && nextParen < nextClosed){
+					nextStart = nextParen + 1;
+					nextClosed = nextParen + 1;
+				}
+				else{
+					contin = false;
+				}
+			}
+			if(!contin){
+				// just a check to make sure when there is an error it doesn't go into here
+				stat1 = statement.substr(0, firstParen);
+				stat2 = statement.substr(firstParen + 1, nextClosed);
+				
+			}
+		}
+		else{
+
+		}
 		
 	}
 	else{
