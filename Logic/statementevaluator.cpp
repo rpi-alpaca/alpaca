77072556b717b06784f7e0c3079fcc879b4f5d64
Hali Cai <Hali Cai>
2019-10-04 17:25:39 -0400

    Added printTruthTable

--- Logic/statementevaluator.cpp
+++ Logic/statementevaluator.cpp
@@ -1,19 +1,32 @@
 
-
 #include "statement.h"
 #include "statementevaluator.h"
 
 
+bool StatementEvaluator::evaluateStatement(StatementParser& s, std::vector<std::pair<std::string, bool> >& variableTruthValues) const{
+	std::unordered_map<std::string, bool> variableValues;
+	for(int i = 0; i < variableTruthValues.size(); i++){
+		variableValues[variableTruthValues[i].first] = variableTruthValues[i].second;
+	}
 
-bool StatementEvaluator::evaluateStatement(StatementParser& s, std::unordered_map<std::string, bool>& variableValues) const{
 	bool isTrue = evaluateBranch(s.head, variableValues);
 	return isTrue;
 }
 
 
+void StatementEvaluator::printTruthTable(const StatementParser& s, const std::vector<std::string> variableNames) const{
+	std::vector<std::pair<std::string, bool> > variableTruthValues;
+	for(int i = 0; i < variableNames.size(); i++){
+		variableTruthValues.push_back(std::make_pair(variableNames[i], true));
+	}
+
+	recurseDownArray(s, variableTruthValues, 0);
+}
+
+
 bool StatementEvaluator::evaluateBranch(StatementNode* p, std::unordered_map<std::string, bool>& variableValues) const{
 	//Node is not an operation (variable)
-	if(p -> opType == 'v'){
+	/*if(p -> opType == 'v'){
 		return variableValues[p -> value];
 	}
 	else if(p -> opType == '~'){
@@ -22,9 +35,19 @@ bool StatementEvaluator::evaluateBranch(StatementNode* p, std::unordered_map<std
 	else{
 		std::function<bool(bool,bool)> operation = functionMap.find(p -> opType) -> second;
 		return operation(evaluateBranch(p -> left, variableValues), evaluateBranch(p-> right, variableValues));
+	}*/
+	return true;
+}
+
+
+void StatementEvaluator::recurseDownArray(const StatementParser& s, std::vector<std::pair<std::string, bool> >& variableTruthValues, int index) const{
+	if(index == variableTruthValues.size()){
+
+	}
+	else{
+		recurseDownArray(s, variableTruthValues, index + 1);
+		variableTruthValues[index].second = false;
+		recurseDownArray(s, variableTruthValues, index + 1);
+		variableTruthValues[index].second = true;
 	}
 }
-/*
-bool StatementEvaluator::printTruthTable(const StatementParser& s, const std::unordered_map<std::string, bool>& variableValues) const{
-	
-}*/
