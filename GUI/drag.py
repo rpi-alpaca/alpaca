6df6cd9e6eec942045b765a59f1f66ee1f00afcc
Chris Moore <moorecrs9@gmail.com>
2019-10-15 16:16:35 -0400

    With NoTransition

--- GUI/drag.py
+++ GUI/drag.py
@@ -29,66 +29,6 @@ class MyScatterLayout(ScatterLayout):
             self.pos = x, y
             return super(MyScatterLayout, self).on_touch_up(touch)
 
-    def transform_with_touch(self, touch):
-        changed = False
-        x = self.bbox[0][0]
-        y = self.bbox[0][1]
-        width = self.bbox[1][0]
-        height = self.bbox[1][1]
-        mid_x = x + width / 2
-        mid_y = y + height / 2
-        inner_width = width * 0.5
-        inner_height = height * 0.5
-        left = mid_x - (inner_width / 2)
-        right = mid_x + (inner_width / 2)
-        top = mid_y + (inner_height / 2)
-        bottom = mid_y - (inner_height / 2)
-
-            # just do a simple one finger drag
-        if len(self._touches) == self.translation_touches:
-            # _last_touch_pos has last pos in correct parent space,
-            # just like incoming touch
-            dx = (touch.x - self._last_touch_pos[touch][0]) \
-                 * self.do_translation_x
-            dy = (touch.y - self._last_touch_pos[touch][1]) \
-                 * self.do_translation_y
-            dx = dx / self.translation_touches
-            dy = dy / self.translation_touches
-            if (touch.x > left and touch.x < right and touch.y < top and touch.y > bottom or self.move_lock) and not self.scale_lock_left and not self.scale_lock_right and not self.scale_lock_top and not self.scale_lock_bottom:
-                self.move_lock = True
-                self.apply_transform(Matrix().translate(dx, dy, 0))
-                changed = True
-
-        change_x = touch.x - self.prev_x
-        change_y = touch.y - self.prev_y
-        anchor_sign = 1
-        sign = 1
-        if abs(change_x) >= 9 and not self.move_lock and not self.scale_lock_top and not self.scale_lock_bottom:
-            if change_x < 0:
-                sign = -1
-            if (touch.x < left or self.scale_lock_left) and not self.scale_lock_right:
-                self.scale_lock_left = True
-                self.pos = (self.pos[0] + (sign * 10), self.pos[1])
-                anchor_sign = -1
-            elif (touch.x > right or self.scale_lock_right) and not self.scale_lock_left:
-                self.scale_lock_right = True
-            self.size[0] = self.size[0] + (sign * anchor_sign * 10)
-            self.prev_x = touch.x
-            changed = True
-        if abs(change_y) >= 9 and not self.move_lock and not self.scale_lock_left and not self.scale_lock_right:
-            if change_y < 0:
-                sign = -1
-            if (touch.y > top or self.scale_lock_top) and not self.scale_lock_bottom:
-                self.scale_lock_top = True
-            elif (touch.y < bottom or self.scale_lock_bottom) and not self.scale_lock_top:
-                self.scale_lock_bottom = True
-                self.pos = (self.pos[0], self.pos[1] + (sign * 10))
-                anchor_sign = -1
-            self.size[1] = self.size[1] + (sign * anchor_sign * 10)
-            self.prev_y = touch.y
-            changed = True
-        return changed
-
     def on_touch_down(self, touch):
         x, y = touch.x, touch.y
         self.prev_x = touch.x
