42c24018b2ed5fadaf657cc23bfe3570384cf357
TLouison <toddmlouison@gmail.com>
2019-10-18 18:36:39 -0400

    Implemented helpscreen in code
    Broke out helpscreen into both helpscreen.py and helpscreen.kv, which now handles
    building the GUI elements and handling screen transitions through a callback executed
    by the button. The main.py file no longer loads the builder file for the help screen,
    that is now handled within the class file itself. Finally, the .kv file was stripped
    of most lines that pertained to object creation, and is now mostly being used for
    object styling.

--- GUI/helpscreen.py
+++ GUI/helpscreen.py
@@ -1,23 +1,48 @@
 from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.uix.tabbedpanel import TabbedPanelItem
 from kivy.uix.tabbedpanel import TabbedPanelHeader
+
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.stacklayout import StackLayout
+
 from kivy.uix.screenmanager import Screen
 from kivy.uix.button import Button
+from kivy.graphics import Rectangle, Color
+
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
+
+Builder.load_file('helpscreen.kv')
 
 class HelpPanelsHeader(TabbedPanelHeader):
-  def __init__(self, **kwargs):
-    super(TabbedPanelHeader, self).__init__(**kwargs)
-    self.background_color = [0,1,1,1]
+	pass
 
 class HelpPanels(TabbedPanel):
-  def __init__(self, **kwargs):
-    super(HelpPanels, self).__init__(**kwargs)
+	pass
+
+class NavigationBar(BoxLayout):
+	pass
 
 # Main class for the help menu to build on top of
 class HelpScreen(Screen):
-  def __init__(self, **kwargs):
-    super(HelpScreen, self).__init__(**kwargs)
+	def __init__(self, **kwargs):
+		super().__init__(**kwargs)
 
-    panels = HelpPanels()
-    header = HelpPanelsHeader()
+		layout = StackLayout()
+		infoPanels = HelpPanels()
+
+		navBar = NavigationBar()
+		returnButton = Button(text="Return to home")
+		returnButton.bind(on_press=self.returnHome)
+		navBar.add_widget(returnButton)
+
+		layout.add_widget(infoPanels)
+		layout.add_widget(navBar)
+		self.add_widget(layout)
+
+	def returnHome(self, instance):
+		self.manager.transition.direction = 'up'
+		self.manager.current = 'Menu'
+		
+    
