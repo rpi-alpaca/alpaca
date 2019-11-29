6ec8b14a7d78ce766780cfd55690042dc5c46f60
Prasant Acharya <setinadi@gmail.com>
2019-11-28 20:24:41 -0500

    Reading and writing from files
    Added a basic stuff to read and write to logic files. Will add more stuff later

---
+++ GUI/NewScreen.py
@@ -0,0 +1,15 @@
+# existing screen project files
+class readFiles:
+    def __init__(self):
+        return
+    def parse(file):
+        # this function parses a logic file and can give it to the new project screen
+        return
+
+class writeFiles:
+    def __init__(self):
+        return
+
+    def write(file):
+        # appends data to a specific file
+        return
